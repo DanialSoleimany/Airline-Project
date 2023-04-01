ce799b19491b1de79b3cbf78428d810139844ffd
Danial <kingofbbx@gmail.com>
2023-04-02 01:14:04 +0430

    Data for Date 1402-01-01 to 1402-01-08

--- Airline-Project.py
+++ Airline-Project.py
@@ -1,15 +1,23 @@
 import requests
 import json
+
 requests.packages.urllib3.disable_warnings()
 
+count = 1
+for i in range(1, 9):
 
-date = '1402-01-01'
-airline = 'irm'
-url = 'https://www.airport.ir/NetForm/Service/fids?'
-result = url+'date={}&airline={}&AUTH_TOKEN=9890071'.format(date,airline)
-print(result)
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+    date = "1402-01-01"
+    listed_date = list(date)
+    listed_date[-1] = str(count)
+    date = "".join(listed_date)
+    airline = "irz" # ایرلاین ساها
+    url = "https://www.airport.ir/NetForm/Service/fids?"
+    result = f"{url}date={date}&airline={airline}&AUTH_TOKEN=9890071"
+    print(result)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36", "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+    response = requests.get(result, headers = headers ,verify = False)
+    data = json.loads(response.text)
+    
+    count += 1
 
-response = requests.get(result,headers=headers,verify = False)
-data = json.loads(response.text)
-print(data)
+    print(data)
